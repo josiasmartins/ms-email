45e18111a1905c448b7ac74021e94d1e2a4b27f8
Caitano <josiasmartinscaitano@gmail.com>
2022-04-18 09:09:26 -0300

    feature: implementado a configuração de envio de email

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -1,6 +1,7 @@
 package com.ms.email.services;
 
 import com.ms.email.EmailRepository;
+import com.ms.email.models.EmailModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +11,8 @@ public class EmailService  {
     // Autowired: para fazer a persistencia com o banco. Get, put, delete,
     @Autowired
     EmailRepository emailRepository;
+
+    public void sendEmail(EmailModel emailModel) {
+
+    }
 }
